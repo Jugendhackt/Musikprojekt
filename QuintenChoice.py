db7c254c09698cd0e2214b5bd6dd3df22df06f5f
Wylee <Wyu@example.com>
2022-11-05 18:05:11 +0100

    Positionierungen optimiert, Stop Button

--- QuintenChoice.py
+++ QuintenChoice.py
@@ -1,107 +1,115 @@
 from tkinter import *
+def configureSong():
 
-chosenTone = 0
-chosenDrum = 0
-chosenSpeed = 0
-myFont = ("cursive 15 bold")
+    chosenTone = 0
+    chosenDrum = 0
+    chosenSpeed = 0
+    myFont = ("cursive 15 bold")
 
-def chooseTone(tone):
-    chosenTone = tone
+    def chooseTone(tone):
+        chosenTone = tone
 
-def chooseDrum(drum):
-    chosenDrum = drum
+    def chooseDrum(drum):
+        chosenDrum = drum
 
-def chooseSpeed(speed):
-    chosenSpeed =speed
+    def chooseSpeed(speed):
+        chosenSpeed =speed
 
-def playTrack(tone, drums, speed):
-    a = 1
-    #An Backend senden
-
-
-win = Tk()
-win.geometry("1500x1000")
-canvas = Canvas(win, bg="#c3ceda", height = "1000", width = "1500")
-
-canvas.create_rectangle(250,100,1250,700, fill = "#738fa7")
-canvas.pack()
-
-canvas.create_text(750, 40, text ="Configure Your SONG", fill="black", font=("Arial 37 bold"))
-canvas.create_text(500, 170, text ="Quintenzirkel", fill="black", font=("Arial 20 bold"))
-
-buttonC = Button(win, text = "C", font=myFont, command = chooseTone("C"), anchor = CENTER)
-buttonC.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonC_window = canvas.create_window(500, 200, anchor=N, window=buttonC)
-
-buttonG = Button(win, text = "G", font=myFont, command = chooseTone("G"), anchor = CENTER)
-buttonG.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonG_window = canvas.create_window(570, 230, anchor=N, window=buttonG)
-
-buttonD = Button(win, text = "D", font=myFont, command = chooseTone("D"), anchor = CENTER)
-buttonD.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonD_window = canvas.create_window(625, 280, anchor=N, window=buttonD)
+    def playTrack(tone, drums, speed):
+        a = 1
+        #An Backend senden
+    def stopTrack(tone, drums, speed):
+        a = 1
 
 
+    win = Tk()
+    win.geometry("1500x1000")
+    canvas = Canvas(win, bg="#c3ceda", height = "1000", width = "1500")
 
-buttonA = Button(win, text = "A", font=myFont, command = chooseTone("A"), anchor = CENTER)
-buttonA.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonA_window = canvas.create_window(650, 350, anchor=N, window=buttonA)
+    canvas.create_rectangle(250,100,1250,700, fill = "#738fa7")
+    canvas.pack()
 
-buttonE = Button(win, text = "E", font=myFont, command = chooseTone("E"), anchor = CENTER)
-buttonE.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonE_window = canvas.create_window(625, 420, anchor=N, window=buttonE)
+    canvas.create_text(750, 40, text ="Configure Your SONG", fill="black", font=("Arial 37 bold"))
+    canvas.create_text(500, 170, text ="Quintenzirkel", fill="black", font=("Arial 20 bold"))
 
-buttonH = Button(win, text = "H", font=myFont, command = chooseTone("H"), anchor = CENTER)
-buttonH.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonH_window = canvas.create_window(570, 470, anchor=N, window=buttonH)
+    buttonC = Button(win, text = "C", font=myFont, command = chooseTone("C"), anchor = CENTER)
+    buttonC.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonC_window = canvas.create_window(500, 200, anchor=N, window=buttonC)
+
+    buttonG = Button(win, text = "G", font=myFont, command = chooseTone("G"), anchor = CENTER)
+    buttonG.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonG_window = canvas.create_window(570, 230, anchor=N, window=buttonG)
+
+    buttonD = Button(win, text = "D", font=myFont, command = chooseTone("D"), anchor = CENTER)
+    buttonD.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonD_window = canvas.create_window(625, 280, anchor=N, window=buttonD)
 
 
 
-buttonE = Button(win, text = "Fis", font=myFont, command = chooseTone("Fis"), anchor = CENTER)
-buttonE.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonE_window = canvas.create_window(500, 500, anchor=N, window=buttonE)
+    buttonA = Button(win, text = "A", font=myFont, command = chooseTone("A"), anchor = CENTER)
+    buttonA.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonA_window = canvas.create_window(650, 350, anchor=N, window=buttonA)
 
-buttonDes = Button(win, text = "Des", font=myFont, command = chooseTone("Des"), anchor = CENTER)
-buttonDes.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonDes_window = canvas.create_window(430, 470, anchor=N, window=buttonDes)
+    buttonE = Button(win, text = "E", font=myFont, command = chooseTone("E"), anchor = CENTER)
+    buttonE.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonE_window = canvas.create_window(625, 420, anchor=N, window=buttonE)
 
-buttonAs = Button(win, text = "As", font=myFont, command = chooseTone("As"), anchor = CENTER)
-buttonAs.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonAs_window = canvas.create_window(375, 420, anchor=N, window=buttonAs)
-
-
-
-buttonEs = Button(win, text = "Es", font=myFont, command = chooseTone("Es"), anchor = CENTER)
-buttonEs.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonEs_window = canvas.create_window(350, 350, anchor=N, window=buttonEs)
-
-buttonB = Button(win, text = "B", font=myFont, command = chooseTone("B"), anchor = CENTER)
-buttonB.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonB_window = canvas.create_window(375, 280, anchor=N, window=buttonB)
-
-buttonF = Button(win, text = "F", font=myFont, command = chooseTone("F"), anchor = CENTER)
-buttonF.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
-buttonF_window = canvas.create_window(430, 230, anchor=N, window=buttonF)
-
-buttonDrum1 = Button(win, text = "Drum One", font=myFont, command = chooseDrum("Drum1"), anchor = CENTER)
-buttonDrum1.configure(width = 8, activebackground = "#738fa7", relief = FLAT)
-buttonDrum1_window = canvas.create_window(950, 200, anchor=NW, window=buttonDrum1)
-
-buttonDrum2 = Button(win, text = "Drum Two", font=myFont, command = chooseDrum("Drum2"), anchor = CENTER)
-buttonDrum2.configure(width = 8, activebackground = "#738fa7", relief = FLAT)
-buttonDrum2_window = canvas.create_window(1000, 250, anchor=N, window=buttonDrum2)
-
-buttonDrum3 = Button(win, text = "Drum Three", font=myFont, command = chooseDrum("Drum3"), anchor = CENTER)
-buttonDrum3.configure(width = 9, activebackground = "#738fa7", relief = FLAT)
-buttonDrum3_window = canvas.create_window(1000, 300, anchor=N, window=buttonDrum3)
+    buttonH = Button(win, text = "H", font=myFont, command = chooseTone("H"), anchor = CENTER)
+    buttonH.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonH_window = canvas.create_window(570, 470, anchor=N, window=buttonH)
 
 
 
-playButton = Button(win, text = "Play", command = playTrack(chosenTone, chosenDrum, chosenSpeed), anchor = W)
-playButton.configure(width = 4, activebackground = "#738fa7", relief = FLAT)
-playButton_window = canvas.create_window(750, 600, anchor=N, window=playButton)
+    buttonE = Button(win, text = "Fis", font=myFont, command = chooseTone("Fis"), anchor = CENTER)
+    buttonE.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonE_window = canvas.create_window(500, 500, anchor=N, window=buttonE)
+
+    buttonDes = Button(win, text = "Des", font=myFont, command = chooseTone("Des"), anchor = CENTER)
+    buttonDes.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonDes_window = canvas.create_window(430, 470, anchor=N, window=buttonDes)
+
+    buttonAs = Button(win, text = "As", font=myFont, command = chooseTone("As"), anchor = CENTER)
+    buttonAs.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonAs_window = canvas.create_window(375, 420, anchor=N, window=buttonAs)
 
 
 
-win.mainloop()
+    buttonEs = Button(win, text = "Es", font=myFont, command = chooseTone("Es"), anchor = CENTER)
+    buttonEs.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonEs_window = canvas.create_window(350, 350, anchor=N, window=buttonEs)
 
+    buttonB = Button(win, text = "B", font=myFont, command = chooseTone("B"), anchor = CENTER)
+    buttonB.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonB_window = canvas.create_window(375, 280, anchor=N, window=buttonB)
+
+    buttonF = Button(win, text = "F", font=myFont, command = chooseTone("F"), anchor = CENTER)
+    buttonF.configure(width = 3, height = 1, activebackground = "#738fa7", relief = FLAT)
+    buttonF_window = canvas.create_window(430, 230, anchor=N, window=buttonF)
+
+
+
+    buttonDrum1 = Button(win, text = "Drum One", font=myFont, command = chooseDrum("Drum1"), anchor = CENTER)
+    buttonDrum1.configure(width = 8, activebackground = "#738fa7", relief = FLAT)
+    buttonDrum1_window = canvas.create_window(941, 200, anchor=NW, window=buttonDrum1)
+
+    buttonDrum2 = Button(win, text = "Drum Two", font=myFont, command = chooseDrum("Drum2"), anchor = CENTER)
+    buttonDrum2.configure(width = 8, activebackground = "#738fa7", relief = FLAT)
+    buttonDrum2_window = canvas.create_window(994, 250, anchor=N, window=buttonDrum2)
+
+    buttonDrum3 = Button(win, text = "Drum Three", font=myFont, command = chooseDrum("Drum3"), anchor = CENTER)
+    buttonDrum3.configure(width = 9, activebackground = "#738fa7", relief = FLAT)
+    buttonDrum3_window = canvas.create_window(1000, 300, anchor=N, window=buttonDrum3)
+
+
+
+    playButton = Button(win, text = "Play", font = myFont, command = playTrack(chosenTone, chosenDrum, chosenSpeed), anchor = W)
+    playButton.configure(width = 4, activebackground = "#738fa7", relief = FLAT)
+    playButton_window = canvas.create_window(750, 630, anchor=N, window=playButton)
+
+    stopButton = Button(win, text = "Stop", font = myFont, command = stopTrack(chosenTone, chosenDrum, chosenSpeed), anchor = W)
+    stopButton.configure(width = 4, activebackground = "#738fa7", relief = FLAT)
+    stopButton_window = canvas.create_window(820, 630, anchor=N, window=stopButton)
+
+    win.mainloop()
+
+configureSong()
