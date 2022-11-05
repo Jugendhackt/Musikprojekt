11e184db86bbc48febf88a0e71f4a5d7e77670fe
BlitzBenedikt <117511666+BlitzBenedikt@users.noreply.github.com>
2022-11-05 17:42:42 +0100

    erster back end commit

---
+++ synthesizer_test.py
@@ -0,0 +1,13 @@
+from synthesizer import Player, Synthesizer, Waveform
+
+player = Player()
+player.open_stream()
+synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
+player.play_wave(synthesizer.generate_constant_wave(440.0, 3.0))
+
+
+chord = [440.0, 550.0, 660.0]
+player.play_wave(synthesizer.generate_chord(chord, 3.0))
+
+from playsound import playsound
+playsound('')
