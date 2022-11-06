4af1beb3ee7d9c3d1a401857e71046f8186dc91c
BlitzBenedikt <117511666+BlitzBenedikt@users.noreply.github.com>
2022-11-06 11:23:09 +0100

    neuer loop

--- loop.py
+++ loop.py
@@ -8,13 +8,28 @@ from playsound import playsound
 #def playprocess(sound):
 #    print(sound)
 #    playsound(sound)
-
+instrument1Beats=3
+instrument2Beats=2
+instrument4Beats=6
+instrument3Beats=16
+instrument5Beats=4
 def play1():
     playsound("OS_ELK_SFX22.wav")
                     
 def play2():
     playsound("OS_ELK_C_Synth_Instrument_Hit_4.wav")
 
+instrument1= euclidean_rhythm(instrument1Beats,16)
+instrument2= euclidean_rhythm(instrument2Beats,16)
+instrument3= euclidean_rhythm(instrument3Beats,16)
+instrument4= euclidean_rhythm(instrument4Beats,16)
+instrument5= euclidean_rhythm(instrument5Beats,16)
+
+i1=0
+i2=0
+i3=0
+i4=0
+i5=0
 
 if __name__ == '__main__':
     zähler=0
@@ -24,12 +39,46 @@ if __name__ == '__main__':
     pulses= 50
 
     while play == True:
-        soundtest=euclidean_rhythm (beats,pulses)
-        for i in range (0, len(soundtest)) :
-            time.sleep(1)
-            if (soundtest[i] == 1):
-                P1 = Process(target=playsound, args=("OS_ELK_SFX22.wav",))
-                P2 = Process(target=playsound, args=("OS_ELK_C_Synth_Instrument_Hit_4.wav",))
+        if (instrument1[i1]==1):
+            P1=Process(target=playsound, args=("OS_ELK_Snare 20.wav",))
+            P1.start()
+        if (instrument2[i2]==1):
+            P2=Process(target=playsound, args=("OS_ELK_C_Synth Instrument Hit 2.wav",))
+            P2.start()
+        if (instrument3[i3]==1):
+            P3=Process(target=playsound, args=("OS_ELK_SFX 22.wav",))
+            P3.start()
+        if (instrument4[i4]==1):
+            P4=Process(target=playsound, args=("OS_ELK_SFX 44.wav",))
+            P4.start()
+        if (instrument5[i5]==1):
+            P5 =Process(target=playsound, args=("bass_a.wav",))
+            P5.start()
+        i1=+1
+        i2=+1
+        i3=+1
+        i4=+1
+        i5=+1
+        
+        if (i1== len(instrument1)):
+            i1 ==0
+        if (i2== len(instrument1)):
+            i2 ==0
+        if (i3== len(instrument1)):
+            i3 ==0
+        if (i3== len(instrument1)):
+            i3 ==0
+        if (i4== len(instrument1)):
+            i4 ==0
+        if (i5== len(instrument1)):
+            i5 ==0
+            
+        
+        #for i in range (0, len(soundtest)) :
+           # time.sleep(1)
+           # if (soundtest[i] == 1):
+               # P1 = Process(target=playsound, args=("OS_ELK_SFX22.wav",))
+               # P2 = Process(target=playsound, args=("OS_ELK_C_Synth_Instrument_Hit_4.wav",))
                 
                 #P1 = Process(name="sound1", target=playsound, args=("OS_ELK_SFX22.wav",))
                 #P2 = Process(name="sound2", target=playsound, args=("OS_ELK_C_Synth_Instrument_Hit_4.wav",))
@@ -37,9 +86,11 @@ if __name__ == '__main__':
                 #P1 = Process(target=play1)
                 #P2 = Process(target=play2)
                  
-                
-                P2.start()
-                P1.start()
+                #P5.start()
+                #P4.start()
+                #P3.start()
+               # P2.start()
+                #P1.start()
                             
                             
                     
